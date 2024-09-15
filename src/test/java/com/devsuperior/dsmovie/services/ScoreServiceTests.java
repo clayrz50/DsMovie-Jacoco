2f9d3f9e993f245336a0257be3a9b9bab67686a4
Clayton <clayrz50@gmail.com>
2024-09-15 00:32:52 -0300

    Finished Test->implemented ScoreServiceTest and UserServiceTest

--- src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
@@ -1,21 +1,92 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.entities.ScoreEntity;
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import com.devsuperior.dsmovie.tests.ScoreFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
 
 @ExtendWith(SpringExtension.class)
 public class ScoreServiceTests {
-	
+
 	@InjectMocks
 	private ScoreService service;
+
+	@Mock
+	private UserService userService;
+
+	@Mock
+	private MovieRepository movieRepository;
+
+	@Mock
+	private ScoreRepository scoreRepository;
 	
+	private Long existingMovieId;
+	private Long nonExistingMovieId;
+	private ScoreDTO scoreDTO;
+	private UserEntity userEntity;
+	private MovieEntity movieEntity;
+	private ScoreEntity scoreEntity;
+	
+	@BeforeEach
+    void setUp() throws Exception {
+        existingMovieId = 1L;
+        nonExistingMovieId = 100L;
+
+        // Criando os mocks necessários
+        scoreDTO = ScoreFactory.createScoreDTO();
+
+        userEntity = UserFactory.createUserEntity();
+
+        movieEntity = MovieFactory.createMovieEntity();
+
+        scoreEntity = ScoreFactory.createScoreEntity();
+
+        // Configurando os mocks
+        Mockito.when(userService.authenticated()).thenReturn(userEntity);
+
+        Mockito.when(movieRepository.findById(existingMovieId)).thenReturn(Optional.of(movieEntity));
+        Mockito.when(movieRepository.findById(nonExistingMovieId)).thenReturn(Optional.empty());
+
+        Mockito.when(scoreRepository.saveAndFlush(any(ScoreEntity.class))).thenReturn(scoreEntity);
+        Mockito.when(movieRepository.save(any(MovieEntity.class))).thenReturn(movieEntity);
+    }
+
 	@Test
 	public void saveScoreShouldReturnMovieDTO() {
+		MovieDTO result = service.saveScore(scoreDTO);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(existingMovieId, result.getId());
+        Assertions.assertEquals("Test Movie", result.getTitle());
+        
 	}
-	
+
 	@Test
 	public void saveScoreShouldThrowResourceNotFoundExceptionWhenNonExistingMovieId() {
+		scoreDTO = new ScoreDTO(nonExistingMovieId, 4.5);
+
+		 Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+	            service.saveScore(scoreDTO);
+	        });
 	}
 }
--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,10 +1,26 @@
 package com.devsuperior.dsmovie.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.projections.UserDetailsProjection;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserDetailsFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
@@ -13,19 +29,62 @@ public class UserServiceTests {
 	@InjectMocks
 	private UserService service;
 
+	@Mock
+	private UserRepository repository;
+
+	@Mock
+	private CustomUserUtil userUtil;
+
+	private String validUsername = "maria@gmail.com";
+	private String invalidUsername = "joao@gmail.com";
+	private UserEntity userEntity;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		userEntity = UserFactory.createUserEntity();
+
+		Mockito.when(userUtil.getLoggedUsername()).thenReturn(validUsername);
+
+		Mockito.when(repository.findByUsername(validUsername)).thenReturn(Optional.of(userEntity));
+		Mockito.when(repository.findByUsername(invalidUsername)).thenReturn(Optional.empty());
+	}
+
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+		UserEntity result = service.authenticated();
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(validUsername, result.getUsername());
 	}
 
 	@Test
 	public void authenticatedShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+		Mockito.when(userUtil.getLoggedUsername()).thenReturn(invalidUsername);
+		Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+			service.authenticated();
+		});
 	}
 
 	@Test
 	public void loadUserByUsernameShouldReturnUserDetailsWhenUserExists() {
+		List<UserDetailsProjection> projections = UserDetailsFactory.createCustomAdminClientUser(validUsername);
+
+		Mockito.when(repository.searchUserAndRolesByUsername(validUsername)).thenReturn(projections);
+
+		UserDetails result = service.loadUserByUsername(validUsername);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(validUsername, result.getUsername());
+		Assertions.assertEquals(projections.get(0).getPassword(), result.getPassword());
+		Assertions.assertFalse(result.getAuthorities().isEmpty());
 	}
 
 	@Test
 	public void loadUserByUsernameShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+		Mockito.when(repository.searchUserAndRolesByUsername(invalidUsername)).thenReturn(List.of());
+
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            service.loadUserByUsername(invalidUsername);
+        });
 	}
 }
